9fa87927894a0857b9b3563a0175a4ce2979c013
Khan <Kaspi123>
2022-08-29 00:56:03 +0600

    adding activity lifecycle log for MainActivity.kt

--- app/src/main/java/com/khanaktas/geoquiz/MainActivity.kt
+++ app/src/main/java/com/khanaktas/geoquiz/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.khanaktas.geoquiz
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.khanaktas.geoquiz.model.Question
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,6 +31,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
         setContentView(R.layout.activity_main)
 
         questionTextView = findViewById(R.id.question_text_view)
@@ -63,6 +67,31 @@ class MainActivity : AppCompatActivity() {
         }
 
         updateQuestion()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
     }
 
     private fun updateQuestion() {
